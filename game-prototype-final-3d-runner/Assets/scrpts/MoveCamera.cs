72ceadd5aa7995688e953f818577f4a829d30921
Zino Adidi <zinoadidi@gmail.com>
2019-05-21 10:36:15 +0300

    basic setup: road & walls

--- game-prototype-final-3d-runner/Assets/scrpts/MoveCamera.cs
+++ game-prototype-final-3d-runner/Assets/scrpts/MoveCamera.cs
@@ -7,7 +7,9 @@ public class MoveCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //code to move camera
         GetComponent<Rigidbody>().velocity = new Vector3(0,0,4);
+
         
     }
 
