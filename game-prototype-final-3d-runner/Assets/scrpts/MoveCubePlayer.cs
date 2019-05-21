d06d85fcee1eff21880d65c6b0dfee24cc1313e2
Zino Adidi <zinoadidi@gmail.com>
2019-05-21 11:58:20 +0300

    basic setup: added player and player controls

---
+++ game-prototype-final-3d-runner/Assets/scrpts/MoveCubePlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCubePlayer : MonoBehaviour
+{
+
+    public KeyCode MoveCubePlayerLeftKey;
+    
+    public KeyCode MoveCubePlayerRightKey;
+
+    public float HorizontalVelocity = 0;
+  
+    public int LaneTracker = 2;
+    public string ControlLock = "no";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody> ().velocity = new Vector3 (HorizontalVelocity, 0, 4);
+       
+        if((Input.GetKeyDown (MoveCubePlayerLeftKey)) && (LaneTracker > 1) && (ControlLock == "no")){
+            Debug.Log("linetracker before left:"+LaneTracker);
+             
+            HorizontalVelocity = -2;
+            StartCoroutine(stopSlide());
+            LaneTracker -=1;
+            Debug.Log("linetracker left:"+LaneTracker);
+            ControlLock = "yes";
+
+        }
+        if((Input.GetKeyDown (MoveCubePlayerRightKey)) && (LaneTracker < 3) && (ControlLock == "no")){
+            Debug.Log("linetracker before right:"+LaneTracker);
+            
+            HorizontalVelocity = 2;
+            StartCoroutine(stopSlide());
+            LaneTracker +=1;
+            Debug.Log("linetracker right:"+LaneTracker);
+            ControlLock = "yes";
+
+        }
+        
+    }
+
+    IEnumerator stopSlide(){
+        yield return new WaitForSeconds (.5f);
+        HorizontalVelocity = 0;
+        ControlLock = "no";
+
+    }
+}
